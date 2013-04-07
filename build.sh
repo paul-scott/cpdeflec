1bc28b552ace30979e07788c6991788452671ad2
Paul Scott <poogascott@gmail.com>
2013-04-07 21:16:45 +1000

    Cleaned up repository

--- build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-swig -python profile.i
-gcc -c -fPIC profile.c commath.c camera.c pattern.c profile_wrap.c -I/usr/include/python2.6/ -std=c99
-ld -shared -ltiff -lm -lgsl -lgslcblas profile.o commath.o camera.o pattern.o profile_wrap.o -o _profile.so -ltiff
-
-rm profile.o commath.o camera.o pattern.o profile_wrap.o profile_wrap.c
--- build2.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-gcc -o cpdeflec cpdeflec.c commath.c camera.c pattern.c fitting.c -O2 -std=c99 -lm -ltiff -lgsl -lgslcblas 
